9d4066d1c736bfe58d75a531131ea9e1e51e0352
Totoblak90 <tobias.blaksley@hotmail.com>
2022-08-30 16:18:03 -0300

    importe el tema de la app, lo configure y cree los modulos shared

---
+++ 03.Angular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { HeaderPromotionComponent } from './modules/header-promotion/header-promotion.component';
+import { HeaderMobileComponent } from './modules/header-mobile/header-mobile.component';
+import { NewsletterComponent } from './modules/newsletter/newsletter.component';
+import { FooterComponent } from './modules/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HeaderPromotionComponent,
+    HeaderMobileComponent,
+    NewsletterComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
