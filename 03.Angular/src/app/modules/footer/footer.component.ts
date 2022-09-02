436a35587f65c39408627ca68fb04a30fbe58a32
Totoblak90 <tobias.blaksley@hotmail.com>
2022-09-01 22:37:53 -0300

    termine de agregar dinamismo a todos los componentes compartidos de la app

--- 03.Angular/src/app/modules/footer/footer.component.ts
+++ 03.Angular/src/app/modules/footer/footer.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryMapped } from 'src/app/interfaces/categories.interface';
+import {
+  SubCategory,
+  SubCategoryList,
+} from 'src/app/interfaces/sub-categories.interface';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { FiltersService } from 'src/app/services/filters.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -6,9 +13,48 @@ import { environment } from 'src/environments/environment';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
   baseImagePath: string = environment.assetUrl;
-  constructor() {}
+  categories: CategoryMapped[] = [];
+  subCategoriesFiltered: SubCategory[] = [];
+  isLoaded = false;
 
-  ngOnInit(): void {}
+  constructor(
+    private categoriesService: CategoriesService,
+    private filterService: FiltersService
+  ) {
+    this.getCategories();
+  }
+
+  private getCategories(): void {
+    this.categoriesService.getAllCategories().subscribe({
+      next: (categories) => {
+        this.categories = categories;
+        this.getSubCategories(categories);
+      },
+      error: (err) => console.log(err),
+    });
+  }
+
+  private getSubCategories(categories: CategoryMapped[]): void {
+    let subCategoriesLoaded = 0;
+    categories.forEach((c) => {
+      c.title_list.forEach((title) => {
+        this.filterService
+          .filter('sub-categories.json', 'title_list', title)
+          .subscribe({
+            next: (res: SubCategoryList) => {
+              subCategoriesLoaded += Object.keys(res).length;
+              for (let key in res) {
+                this.subCategoriesFiltered.push(res[key]);
+                if (this.subCategoriesFiltered.length === subCategoriesLoaded) {
+                  this.isLoaded = true;
+                }
+              }
+            },
+            error: (err) => console.log(err),
+          });
+      });
+    });
+  }
 }
--- 03.Angular/src/app/modules/header-mobile/header-mobile.component.ts
+++ 03.Angular/src/app/modules/header-mobile/header-mobile.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CategoryMapped } from 'src/app/interfaces/categories.interface';
+import {
+  SubCategory,
+  SubCategoryList,
+} from 'src/app/interfaces/sub-categories.interface';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { FiltersService } from 'src/app/services/filters.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -6,10 +13,48 @@ import { environment } from 'src/environments/environment';
   templateUrl: './header-mobile.component.html',
   styleUrls: ['./header-mobile.component.css'],
 })
-export class HeaderMobileComponent implements OnInit {
+export class HeaderMobileComponent {
   baseImagePath: string = environment.assetUrl;
+  categories: CategoryMapped[] = [];
+  subCategoriesFiltered: SubCategory[] = [];
 
-  constructor() {}
+  constructor(
+    private categoriesService: CategoriesService,
+    private filterService: FiltersService
+  ) {
+    this.getCategories();
+  }
 
-  ngOnInit(): void {}
+  private getCategories(): void {
+    this.categoriesService.getAllCategories().subscribe({
+      next: (categories) => {
+        this.categories = categories;
+        this.getSubCategories(categories);
+      },
+      error: (err) => console.log(err),
+    });
+  }
+
+  private getSubCategories(categories: CategoryMapped[]): void {
+    categories.forEach((c) => {
+      c.title_list.forEach((title) => {
+        this.filterService
+          .filter('sub-categories.json', 'title_list', title)
+          .subscribe({
+            next: (res: SubCategoryList) => {
+              for (let key in res) {
+                this.subCategoriesFiltered.push(res[key]);
+              }
+            },
+            error: (err) => console.log(err),
+          });
+      });
+    });
+  }
+
+  toggleSubMenu(element: HTMLUListElement) {
+    element.style.display === 'block'
+      ? (element.style.display = 'none')
+      : (element.style.display = 'block');
+  }
 }
